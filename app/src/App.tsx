519a92c8dc45c9ce4b15a9bd01d3d9aaa8a6cd65
kanompang962 <kanompang962@gmail.com>
2023-12-11 13:21:33 +0700

    update

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Docker + Remote</h1>
+      <h1>Vite + React + Docker + Remote + Dev</h1>
       <div className="card">
         <button className='btn btn-success' onClick={() => setCount((count) => count + 1)}>
           count is {count}
